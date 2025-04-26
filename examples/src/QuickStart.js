5466ef41eefa6002f1ff40955f4874747938549a
Serhii Khoma <srghma@gmail.com>
2025-04-27 06:46:11 +0700

    fix: eslint and examples, add them to ci

--- examples/src/QuickStart.js
+++ examples/src/QuickStart.js
@@ -1,10 +1,8 @@
-"use strict";
-
-exports.unluckyImpl = function (n) {
+export function unluckyImpl(n) {
   // Unlucky number 13!
   if (n === 13) {
     return null;
   }
 
   return n;
-};
+}
--- src/Data/Nullable.js
+++ src/Data/Nullable.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-eq-null, eqeqeq */
-
 const nullImpl = null;
 export { nullImpl as null };
 export const nullable = (a, r, f) => a == null ? r : f(a);
